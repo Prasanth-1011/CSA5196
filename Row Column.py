9d958989ee513c665825cf07abcb761826f5b211
Prasanth-1011 <112630396+Prasanth-1011@users.noreply.github.com>
2023-11-24 11:00:05 +0530

    Row Column.py

--- Row Column.py
+++ Row Column.py
@@ -4,7 +4,7 @@ p = input("Enter the Plain Text : ")
 col = len(n)
 row = math.ceil(len(p)/col)
 
-b = [[0 for _ in range(col)] for _ in range(row)]
+b = [['X' for _ in range(col)] for _ in range(row)]
 m = 0
 
 for i in range(row):
@@ -13,11 +13,6 @@ for i in range(row):
         m += 1
         if m == len(p):
             break
-        
-for i in range(row):
-    for j in range(col):
-        if b[i][j] == 0:
-            b[i][j] = 'X'
             
 c = ""
 for i in n:
